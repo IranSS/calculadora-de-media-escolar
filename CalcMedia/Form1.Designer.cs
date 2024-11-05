b70f949544e2b08d1536611d3d1ab40a3b1fdc6b
Iran <iranildosousa1234@gmail.com>
2024-11-05 16:56:38 -0300

    AjusteDeEscala

--- CalcMedia/Form1.Designer.cs
+++ CalcMedia/Form1.Designer.cs
@@ -48,7 +48,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(75, 29);
+            this.label1.Location = new System.Drawing.Point(75, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 13);
             this.label1.TabIndex = 0;
@@ -57,10 +57,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 63);
+            this.label2.Location = new System.Drawing.Point(20, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 0;
             this.label2.Text = "Nota 1";
             // 
             // textBox1
@@ -68,14 +68,14 @@
             this.textBox1.Location = new System.Drawing.Point(65, 56);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(65, 82);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 4;
+            this.textBox2.TabIndex = 2;
             // 
             // label3
             // 
@@ -83,7 +83,7 @@
             this.label3.Location = new System.Drawing.Point(20, 85);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(39, 13);
-            this.label3.TabIndex = 3;
+            this.label3.TabIndex = 0;
             this.label3.Text = "Nota 2";
             // 
             // textBox3
@@ -91,15 +91,15 @@
             this.textBox3.Location = new System.Drawing.Point(65, 108);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 6;
+            this.textBox3.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 111);
+            this.label4.Location = new System.Drawing.Point(20, 112);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(39, 13);
-            this.label4.TabIndex = 5;
+            this.label4.TabIndex = 0;
             this.label4.Text = "Nota 3";
             // 
             // textBox4
@@ -107,15 +107,15 @@
             this.textBox4.Location = new System.Drawing.Point(65, 134);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 8;
+            this.textBox4.TabIndex = 4;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(20, 137);
+            this.label5.Location = new System.Drawing.Point(20, 138);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(39, 13);
-            this.label5.TabIndex = 7;
+            this.label5.TabIndex = 0;
             this.label5.Text = "Nota 4";
             // 
             // label6
@@ -124,7 +124,7 @@
             this.label6.Location = new System.Drawing.Point(193, 98);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(57, 13);
-            this.label6.TabIndex = 9;
+            this.label6.TabIndex = 0;
             this.label6.Text = "A média é:";
             // 
             // label7
@@ -133,7 +133,7 @@
             this.label7.Location = new System.Drawing.Point(246, 98);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(16, 13);
-            this.label7.TabIndex = 10;
+            this.label7.TabIndex = 0;
             this.label7.Text = "...";
             // 
             // button1
@@ -141,7 +141,7 @@
             this.button1.Location = new System.Drawing.Point(196, 53);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
+            this.button1.TabIndex = 5;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -151,7 +151,7 @@
             this.button2.Location = new System.Drawing.Point(196, 134);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 12;
+            this.button2.TabIndex = 0;
             this.button2.Text = "Limpar";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -162,7 +162,7 @@
             this.label8.Location = new System.Drawing.Point(193, 115);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(40, 13);
-            this.label8.TabIndex = 13;
+            this.label8.TabIndex = 0;
             this.label8.Text = "Status:";
             // 
             // label9
@@ -171,7 +171,7 @@
             this.label9.Location = new System.Drawing.Point(231, 115);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(16, 13);
-            this.label9.TabIndex = 14;
+            this.label9.TabIndex = 0;
             this.label9.Text = "...";
             // 
             // Form1
@@ -194,6 +194,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(308, 233);
             this.Name = "Form1";
             this.Text = "Calculador de média";
             this.ResumeLayout(false);
